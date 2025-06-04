72453ff16a95736fd7cdc1014a6111ad9d8ec03d
JagguJagadeesh <jaggujagadeesh447@gmail.com>
2025-06-05 00:45:46 +0530

    change in port

--- api/index.ts
+++ api/index.ts
@@ -10,11 +10,20 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080
+const allowedOrigins = [process.env.BASE_URL, 'http://localhost:3000'];
 
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.BASE_URL || 'http://localhost:3000',
+  origin: function(origin, callback){
+    // Allow requests with no origin (like mobile apps or curl)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
 
