2f40a87b9d6ddec5194e44928219f264f8b65bd1
JagguJagadeesh <jaggujagadeesh447@gmail.com>
2025-09-30 00:20:27 +0530

    fix: fixing authentication

--- api/controllers/authController.ts
+++ api/controllers/authController.ts
@@ -34,15 +34,10 @@ const signup = async (req: Request,res: Response) => {
         })
         const token = generateToken(user.id)
 
-        return res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,         
-        sameSite: 'none',    
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        })
+        return res
         .status(200)
         .json({
-            message: `User ${user.name} is Created Successfully`,user:{id:user.id,name:user.name,email:user.email}
+            message: `User ${user.name} is Created Successfully`,token,user:{id:user.id,name:user.name,email:user.email}
         }) as any
 
     } catch (e) {
@@ -67,13 +62,7 @@ const sigin = async (req: Request,res: Response) => {
         if(!verifypass) return res.status(500).json({message:'password is incorret'});
 
         const token = generateToken(checkUser.id)
-
-        return res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,         
-        sameSite: 'none',    
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        }).status(200).json({message:`User ${checkUser.name} is Logined Sucessfully`,user:{id:checkUser.id,name:checkUser.name,email:checkUser.email}}) as any
+        return res.status(200).json({message:`User ${checkUser.name} is Logined Sucessfully`,token,user:{id:checkUser.id,name:checkUser.name,email:checkUser.email}}) as any
 
     } catch (e) {
         console.log('Error at Signup',e)
--- api/index.ts
+++ api/index.ts
@@ -21,8 +21,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.BASE_URL,
-    // origin: 'http://localhost:3000',
+    // origin: process.env.BASE_URL,
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
